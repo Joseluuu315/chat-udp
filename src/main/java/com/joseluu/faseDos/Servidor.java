30bc6580efc23983620a82339a94ebf886abc413
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-12-15 12:33:27 +0100

    Improve: Some change to do a good chat with client-server

---
+++ src/main/java/com/joseluu/faseDos/Servidor.java
@@ -0,0 +1,35 @@
+package com.joseluu.faseDos;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Servidor {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData;
+
+        System.out.println("Servidor UDP para descubrimiento iniciado en puerto 9876...");
+
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+
+            String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            InetAddress clientAddress = receivePacket.getAddress();
+            int clientPort = receivePacket.getPort();
+
+            // Filtrar auto-respuesta
+            if (message.equals("DISCOVER_SERVER") && !clientAddress.equals(InetAddress.getLocalHost())) {
+                System.out.println("Solicitud de descubrimiento recibida de: " + clientAddress);
+
+                String response = "SERVER_HERE:" + InetAddress.getLocalHost().getHostName();
+                sendData = response.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                serverSocket.send(sendPacket);
+            }
+        }
+    }
+}
